d5becd579d80973c66545e0be0b1b2600ebeee55
Ondrej Capek <ocapek@gmail.com>
2026-05-13 10:12:34 +0200

    add excel DONE

--- tests/playground/playground.robot
+++ tests/playground/playground.robot
@@ -4,11 +4,11 @@ Library    ../resources/keywords/KeywordsTypofix.py
 
 *** Test Cases ***
 Data store File Add List
-    FOR    ${i}    IN RANGE     1    10
-        Data Store New Line
+    FOR    ${i}    IN RANGE     1    5
+
         Data Store Add Item     name    Some rule name ${i}
         Data Store Add Item     before    Line before ${i}
-        Data Store Add Item     after    Line after ${i}
+        Data Store Add Item     after    Line after ${i}    new_line=True
     END
     Data Store Save
 
--- tests/resources/keywords/KeywordsTypofix.py
+++ tests/resources/keywords/KeywordsTypofix.py
@@ -8,27 +8,20 @@ from openpyxl import load_workbook
 
 class KeywordsTypofix(object):
     def __init__(self):
-        print("AAAAxxA")
         self.TEST_DATA_DIR = os.path.join(os.getcwd(),  'tests', 'resources', 'test_data')
-        path = os.path.join(self.TEST_DATA_DIR, "DataStore.xlsx")
-        print(path.title())
-        print("AAAAAA")
-        data_store = load_workbook(path)
+        data_store = load_workbook(os.path.join(self.TEST_DATA_DIR, "DataStore.xlsx"))
         self.data_store_list = data_store.copy_worksheet(data_store["_pattern"])
         self.data_store_list.title = datetime.now().strftime('%Y-%m-%d_%H_%M_%S')
-        print(self.data_store_list.title)
-        self.data_store_offset = 1
+        self.data_store_offset = 2
 
-    def data_store_new_line (self):
-        self.data_store_offset+=1
-
-    def data_store_add_item (self, name, value):
+    def data_store_add_item (self, name, value, new_line=False):
         col = 1
         while self.data_store_list.cell(1,col).value:
             if str(self.data_store_list.cell(1,col).value).strip().lower() == name.lower():
                 self.data_store_list.cell(self.data_store_offset, col).value = value
                 break
             col+=1
+        self.data_store_offset = self.data_store_offset + 1 if new_line else self.data_store_offset
 
     def data_store_save(self):
         self.data_store_list.parent.save(os.path.join(self.TEST_DATA_DIR, "DataStore.xlsx"))
