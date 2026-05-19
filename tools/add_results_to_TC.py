61ab76b68dcbcdeaf67abea2fb9a8583785a8835
Ondrej Capek <ocapek@gmail.com>
2026-05-19 20:12:05 +0200

    report first

--- tools/add_results_to_TC.py
+++ tools/add_results_to_TC.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+
+from numpy.ma.core import info
 from openpyxl import load_workbook
 import xml.etree.ElementTree as ET
 
@@ -15,25 +17,11 @@ class Helpers(Common):
 
 if __name__ == "__main__":
     m = Helpers()
-    src = ET.parse(m.REPORT_FILE).getroot()
-    print("---X-")
-    root = ET.parse(m.REPORT_FILE).getroot()
-    status = root.find("./suite/suite/suite/suite/test[@name='44__Guns_N__RosesCzech__academic_rules_']")
+    to_report = ET.parse(m.REPORT_FILE).getroot()
+    status = to_report.find("./suite/suite/suite/suite/test[@name='45__Correct_form_of_C__a_K__in_CzechCzech__academic_rules_']")
     print(status.find('status').attrib['status'])
     print(status.find('status').attrib['start'])
-    print("---X-")
-    root = ET.parse(m.REPORT_FILE).getroot()
-    status = root.find("./suite/suite/suite/suite/test[@name='45__Correct_form_of_C__a_K__in_CzechCzech__academic_rules_']")
-    print(status.find('status').attrib['status'])
-    print(status.find('status').attrib['start'])
-    print("dddddd")
-    msg = root.find("./suite/suite/suite/suite/test[@id='s1-s1-s1-s1-t3']/kw/kw[@name='Element Text Should Be']/msg")
-    print(msg.text)
-    print("---X-")
-    print(msg.attrib)
-    for msg in root.findall("./suite/suite/suite/suite/test[@id='s1-s1-s1-s1-t3']/kw/kw[@name='Element Text Should Be']/msg"):
-        print("---Xxxx-")
-        print(msg.text)
-    msg  = root.findall("./suite/suite/suite/suite/test[@id='s1-s1-s1-s1-t3']/kw/kw[@name='Element Text Should Be']/msg")
-    print("---Xxxxaaa-")
-    print(msg[3].text)
+    msg  = to_report.findall("./suite/suite/suite/suite/test[@id='s1-s1-s1-s1-t3']/kw/kw[@name='Element Text Should Be']/msg")
+    info = msg[3].text.strip().replace('\n', '')
+    info = ' '.join(info.split()).replace("'//*[@role=\"textbox\"]'", '')
+    print(info)
