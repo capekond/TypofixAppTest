aa3a37001554eab3e1ebd72bc4e302779c01c26a
Ondrej Capek <ocapek@gmail.com>
2026-05-19 20:16:42 +0200

    ssasdf

--- tools/add_results_to_TC.py
+++ tools/add_results_to_TC.py
@@ -14,14 +14,22 @@ class Helpers(Common):
     def __init__(self):
         super().__init__()
 
+    def get_results(self, test_names):
+        for test_name in test_names:
+            pass
+
 
 if __name__ == "__main__":
     m = Helpers()
     to_report = ET.parse(m.REPORT_FILE).getroot()
-    status = to_report.find("./suite/suite/suite/suite/test[@name='45__Correct_form_of_C__a_K__in_CzechCzech__academic_rules_']")
+
+    test_name = '45__Correct_form_of_C__a_K__in_CzechCzech__academic_rules_'
+    s1 = "./suite/suite/suite/suite/test[@name='45__Correct_form_of_C__a_K__in_CzechCzech__academic_rules_']"
+    s2 = "./suite/suite/suite/suite/test[@name='45__Correct_form_of_C__a_K__in_CzechCzech__academic_rules_']/kw/kw[@name='Element Text Should Be']/msg"
+    status = to_report.find(s1)
     print(status.find('status').attrib['status'])
     print(status.find('status').attrib['start'])
-    msg  = to_report.findall("./suite/suite/suite/suite/test[@id='s1-s1-s1-s1-t3']/kw/kw[@name='Element Text Should Be']/msg")
+    msg  = to_report.findall(s2)
     info = msg[3].text.strip().replace('\n', '')
     info = ' '.join(info.split()).replace("'//*[@role=\"textbox\"]'", '')
     print(info)
