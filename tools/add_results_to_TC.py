2388f06afa36ec6c739c7beecb74e2dc562ed6bc
Ondrej Capek <ocapek@gmail.com>
2026-05-19 20:04:44 +0200

    report everything

--- tools/add_results_to_TC.py
+++ tools/add_results_to_TC.py
@@ -16,38 +16,24 @@ class Helpers(Common):
 if __name__ == "__main__":
     m = Helpers()
     src = ET.parse(m.REPORT_FILE).getroot()
-    # tests = src.getroot().findall("//test[@name='44__Guns_N__RosesCzech__academic_rules_'")
-    # tests = src.getroot().findall("//test")
-    # tests = src.findall("/robot/suite/suite/suite/suite/test[@name='44__Guns_N__RosesCzech__academic_rules_'")
-    for test in src.iter('test'):
-        print(test.attrib)
-    print("*****")
-
-    tree = ET.parse(m.REPORT_FILE2)
-    root = tree.getroot()
-    #
-    # for movie in root.iter('movie'):
-    #     print(movie.attrib)
-
-    for movie in root.findall("./genre/decade/movie/[year='1992']"):
-        print(movie.attrib)
-
-    print("*****")
-    aaa = root.find("./genre/decade/movie/[year='1992']")
-    print(aaa.attrib)
-
-    print("-----")
-    b2tf = root.find("./genre/decade/movie[@title='Back 2 the Future']")
-    print(b2tf.attrib)
-    print(b2tf.find('format').attrib)
-    print(b2tf.find('format').text)
-    pass
-
     print("---X-")
     root = ET.parse(m.REPORT_FILE).getroot()
-    b2tf = root.find("./suite/suite/suite/suite/test[@name='44__Guns_N__RosesCzech__academic_rules_']")
-    print(b2tf.attrib)
-    # print(b2tf.find('format').attrib)
-    # print(b2tf.find('format').text)
-    pass
-
+    status = root.find("./suite/suite/suite/suite/test[@name='44__Guns_N__RosesCzech__academic_rules_']")
+    print(status.find('status').attrib['status'])
+    print(status.find('status').attrib['start'])
+    print("---X-")
+    root = ET.parse(m.REPORT_FILE).getroot()
+    status = root.find("./suite/suite/suite/suite/test[@name='45__Correct_form_of_C__a_K__in_CzechCzech__academic_rules_']")
+    print(status.find('status').attrib['status'])
+    print(status.find('status').attrib['start'])
+    print("dddddd")
+    msg = root.find("./suite/suite/suite/suite/test[@id='s1-s1-s1-s1-t3']/kw/kw[@name='Element Text Should Be']/msg")
+    print(msg.text)
+    print("---X-")
+    print(msg.attrib)
+    for msg in root.findall("./suite/suite/suite/suite/test[@id='s1-s1-s1-s1-t3']/kw/kw[@name='Element Text Should Be']/msg"):
+        print("---Xxxx-")
+        print(msg.text)
+    msg  = root.findall("./suite/suite/suite/suite/test[@id='s1-s1-s1-s1-t3']/kw/kw[@name='Element Text Should Be']/msg")
+    print("---Xxxxaaa-")
+    print(msg[3].text)
