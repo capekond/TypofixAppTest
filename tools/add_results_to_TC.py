b8f78edcf728f9d3c655296d5c1edb58c758b8cd
Ondrej Capek <ocapek@gmail.com>
2026-05-20 02:57:24 +0200

    some done

--- tools/add_results_to_TC.py
+++ tools/add_results_to_TC.py
@@ -1,15 +1,13 @@
 import xml.etree.ElementTree as Et
-
 import pandas as pd
 from openpyxl import load_workbook
-
 from tests.resources.common import Common
 
 class Helpers(Common):
     def __init__(self):
         super().__init__()
 
-    def get_test_names_from_TC(self) -> list[str]:
+    def get_test_names_from_tc(self) -> list[str]:
         df = pd.read_excel(self.TEST_CASES_FILE, sheet_name=0)
         return df['*** Test Cases ***'].tolist()
 
@@ -23,29 +21,33 @@ class Helpers(Common):
                 s2 = f"./suite/suite/suite/suite/test[@name='{test_name}']/kw/kw[@name='Element Text Should Be']/msg"
                 status = to_report.find(s1)
                 rs.append(status.find('status').attrib['status'])
-                rs.append(status.find('status').attrib['start'])
                 if rs[0] == 'FAIL':
                     msg = to_report.findall(s2)
                     info = msg[3].text.strip().replace('\n', '')
                     rs.append(' '.join(info.split()).replace("'//*[@role=\"textbox\"]'", ''))
                 else:
-                    rs.append('No error for PASS result')
+                    rs.append('')
+                rs.append(status.find('status').attrib['start'])
                 res.append(rs)
             except AttributeError as e:
                 print(f"For test '{test_name}' missing data in {self.REPORT_FILE}")
                 print(e)
                 exit(1)
-
-
         return res
 
-    def write_test_names_from_TC(self, t_results: list[list[str]]) -> None:
-        pass
+    def write_test_names_from_tc(self, t_results: list[list[str]]) -> None:
+        wb = load_workbook(self.TEST_CASES_FILE)
+        sh = wb.worksheets[0]
+        for rid, r in enumerate(t_results):
+            for cid, c in enumerate(r):
+                sh.cell(2 + rid, 9 + cid, c)
+        wb.save(self.TEST_CASES_FILE)
+        wb.close()
 
 if __name__ == "__main__":
     m = Helpers()
-    test_names = m.get_test_names_from_TC()
+    test_names = m.get_test_names_from_tc()
     print(f"Selected {len(test_names)} test cases:")
     print(*test_names, sep='\n')
     test_results = m.get_results(test_names)
-    m.write_test_names_from_TC(test_results)
+    m.write_test_names_from_tc(test_results)
